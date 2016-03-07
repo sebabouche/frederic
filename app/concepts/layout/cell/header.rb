6e13ea005bd1caaeb3898dfacc7f7ff7e4335fd9
sebabouche <s.nicolaidis@me.com>
2016-03-07 08:28:44 +0000

    Separated header view

--- app/concepts/layout/cell/header.rb
+++ app/concepts/layout/cell/header.rb
@@ -1,9 +1,5 @@
 class Layout::Cell::Header < Layout::Cell
   def show
-    " <div class='sn-header' data-uk-parallax='bg: 200'>
-        <div class='sn-header-inner uk-text-center' data-uk-parallax='{opacity:0, viewport: 0.7, y: 50}'>
-          <h1> #{model} </h1>
-        </div>
-      </div>"
+    render :header
   end
 end
